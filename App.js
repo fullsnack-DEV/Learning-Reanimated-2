64e94e2f0cd68f381265ec4fb8f8f063ecd576f7
fullsnack-DEV <pratikpatre2019@gmail.com>
2021-07-03 22:41:18 +0530

    Implmenting  a Nesred Navigators

--- App.js
+++ App.js
@@ -2,16 +2,30 @@ import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import HomePage from "./Pages/HomePage";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomePage from "./Screens/HomeScreen";
+import FavScreen from "./Screens/FavScreen";
+import TickeDetailScreen from "./Screens/TickeDetailScreen";
+import ProfileScreen from "./Screens/ProfileScreen";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const BootomTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="home" component={HomePage} />
+      <Tab.Screen name="fav" component={FavScreen} />
+      <Tab.Screen name="Ticket" component={TickeDetailScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator headerMode={false}>
-        <Stack.Screen name="home" component={HomePage} />
-      </Stack.Navigator>
+      <BootomTabs />
     </NavigationContainer>
   );
 }
